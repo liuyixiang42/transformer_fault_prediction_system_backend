bf842cfdd985e990dc1dfed3ee049f2278c681ca
liuyixiang <576537490@qq.com>
2023-04-17 15:15:53 +0800

    init

--- src/main/java/com/reins/bookstore/TransformerApplication.java
+++ src/main/java/com/reins/bookstore/TransformerApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BookstoreApplication {
+public class TransformerApplication {
 
-    public static void main(String[] args) {SpringApplication.run(BookstoreApplication.class, args);
+    public static void main(String[] args) {SpringApplication.run(TransformerApplication.class, args);
     }
 
 }
